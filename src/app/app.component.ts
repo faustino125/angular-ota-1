1fb9fec00c528c1e70498a9c9681815d450071ee
faustino125 <flrbest@hotmail.com>
2020-04-28 13:07:23 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Usuario, FEMENINO, MASCULINO  } from './componente/usuario/usuario.doma
 })
 export class AppComponent {
   title = 'app'
-  usuarios = [
-    new Usuario('Gabriel Graves', 'Soy el Brad Pitt de Lugano', MASCULINO),
-    new Usuario('Javier Zolotarchuk', 'Tengo el corazón mirando al Sur...', MASCULINO),
-    new Usuario('Clara Allende', 'Git Git Scala Git', FEMENINO)
-  ]
 }
