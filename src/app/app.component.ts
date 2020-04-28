74004e8763531b39603b6657aad42302717b0bd9
faustino125 <flrbest@hotmail.com>
2020-04-28 13:08:11 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Usuario, FEMENINO, MASCULINO  } from './componente/usuario/usuario.domain';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
